cf8ec8b8591922f20f65bab132d3a6eb52a85c66
Paolo Gilberti <42065089+paologilberti@users.noreply.github.com>
2024-07-22 18:57:55 +0200

    Create cifre_pari_di_un_intero_positivo.c

---
+++ 2021-11-03/cifre_pari_di_un_intero_positivo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+    int contatore = 0;
+    int cifra;
+
+    do {
+        printf("Inserisci un numero intero positivo: ");
+        scanf("%d", &numero);
+    } while (numero <= 0);
+
+    while (numero > 0) {
+        cifra = numero % 10;
+
+        if (cifra % 2 == 0)
+            contatore++;
+
+        numero = numero / 10;
+    }
+
+    if (contatore == 1)
+        printf("Il numero inserito ha %d cifra pari\n", contatore);
+    else
+        printf("Il numero inserito ha %d cifre pari\n", contatore);
+
+    return 0;
+}
