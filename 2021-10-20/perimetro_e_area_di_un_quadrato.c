e446cc7fa495a40cf3712aec32ecbe6ded8a3a63
Paolo Gilberti <42065089+paologilberti@users.noreply.github.com>
2024-07-22 18:26:39 +0200

    Update perimetro_e_area_di_un_quadrato.c

--- 2021-10-20/perimetro_e_area_di_un_quadrato.c
+++ 2021-10-20/perimetro_e_area_di_un_quadrato.c
@@ -5,13 +5,13 @@ int main() {
     int perimetro;
     int area;
 
-    printf("Inserisci la lunghezza (in cm) del lato di un quadrato: ");
+    printf("Inserisci la lunghezza del lato di un quadrato (in cm): ");
     scanf("%d", &lato);
 
     perimetro = lato * 4;
     area = lato * lato;
 
-    printf("\nIl perimetro di un quadrato di lato %d cm è uguale a %d cm, mentre l'area è uguale a %d cm\n", lato, perimetro, area);
+    printf("\nIl perimetro di un quadrato di lato %d cm è uguale a %d cm, mentre l'area è uguale a %d cm quadrati\n", lato, perimetro, area);
 
     return 0;
 }
