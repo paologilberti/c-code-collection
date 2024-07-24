aa37f6b98bfd7b1dc92ae4c3674974852d7fa71d
Paolo Gilberti <42065089+paologilberti@users.noreply.github.com>
2024-07-24 23:59:39 +0200

    Create inizializzazione_di_una_matrice_secondo_uno_schema.c

---
+++ 2021-12-01/inizializzazione_di_una_matrice_secondo_uno_schema.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define DIMENSIONE 5
+
+int main() {
+    int numero;
+    int matrice[DIMENSIONE][DIMENSIONE];
+    int i;
+    int j;
+
+    printf("Inserisci un numero intero: ");
+    scan&("%d", &numero);
+
+    for (i = 0; i < DIMENSIONE; i++)
+        for (j = 0; j < DIMENSIONE; j++)
+            if (i < j)
+                matrice[i][j] = j + numero;
+            else
+                matrice[i][j] = i + numero;
+    
+    printf("\n");
+    
+    for (i = 0; i < DIMENSIONE; i++) {
+        for (j = 0; j < DIMENSIONE; j++)
+            printf("%4d", matrice[i][j]);
+        printf("\n");
+    }
+    
+    return 0;
+}
